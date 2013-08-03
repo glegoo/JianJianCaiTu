afa1e2e40a22232eece4c425426ac8a1436691db
w <w@wtekiMac-mini.local>
2013-08-03 10:22:05 +0800

    Initial Commit

---
+++ JianJianCaiTu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JianJianCaiTu
+//
+//  Created by w on 13-8-3.
+//  Copyright (c) 2013年 w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
